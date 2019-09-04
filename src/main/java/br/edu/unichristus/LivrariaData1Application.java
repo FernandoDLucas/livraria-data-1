1fcdf19c4f7ecda57e3448f8dfbb163868d4d72a
Fernando <fernandotiunichristus@gmail.com>
2019-09-03 21:27:55 -0300

    Inicialização do projeto

---
+++ src/main/java/br/edu/unichristus/LivrariaData1Application.java
@@ -0,0 +1,13 @@
+package br.edu.unichristus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaData1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaData1Application.class, args);
+	}
+
+}
